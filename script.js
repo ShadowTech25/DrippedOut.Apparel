a53df587af11b7d31503f20649362d2d68f9a14c
ShadowTech25 <johnnysouth1994@gmail.com>
2025-04-15 16:51:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,15 @@ function updateCart() {
     cartItemsEl.appendChild(li);
   });
 }
+document.addEventListener('DOMContentLoaded', function () {
+  const shopButton = document.querySelector('.cta');
 
+  if (shopButton) {
+    shopButton.addEventListener('click', function () {
+      window.location.href = 'https://shadowtech25.github.io/DrippedOut.Apparel/shop.html';
+    });
+  }
+});
 function checkout() {
   alert('Order placed! Thank you for shopping Dripped Out.');
   cart = [];
